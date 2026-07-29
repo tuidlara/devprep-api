611462dd9fa32ca1173570ee26b6e1c704672de6
Arthur de Lara <arthur.zilli@hotmail.com>
2026-07-29 12:49:26 -0300

    inicializando projeto

---
+++ src/main/java/com/arthur/devprep_api/DevprepApiApplication.java
@@ -0,0 +1,13 @@
+package com.arthur.devprep_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevprepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevprepApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arthur/devprep_api/DevprepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthur.devprep_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevprepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
