c7f952e0f88d4f8b98a22155c1ffb355c29036f2
Arthur de Lara <arthur.zilli@hotmail.com>
2026-07-30 09:23:49 -0300

    feat: adiciona controller

---
+++ src/main/java/com/arthur/devprep_api/controller/QuestionController.java
@@ -0,0 +1,25 @@
+package com.arthur.devprep_api.controller;
+
+import com.arthur.devprep_api.dto.QuestionRequest;
+import com.arthur.devprep_api.dto.QuestionResponse;
+import com.arthur.devprep_api.service.QuestionService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionController {
+
+    private final QuestionService questionService;
+
+    public QuestionController(QuestionService questionService) {
+        this.questionService = questionService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public QuestionResponse createQuestion(@Valid @RequestBody QuestionRequest request) {
+        return questionService.createQuestion(request);
+    }
+}
